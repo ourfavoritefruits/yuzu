45bdc53b8f92f05962e0ddabbe45d657886a1a7e
bunnei <bunneidev@gmail.com>
2018-01-04 13:24:08 -0500

    cmake: Add CopyYuzuUnicornDeps script.

---
+++ CMakeModules/CopyYuzuUnicornDeps.cmake
@@ -0,0 +1,9 @@
+function(copy_yuzu_unicorn_deps target_dir)
+    include(WindowsCopyFiles)
+    set(DLL_DEST "${CMAKE_BINARY_DIR}/bin/$<CONFIG>/")
+    windows_copy_files(${target_dir} ${UNICORN_DLL_DIR} ${DLL_DEST}
+        libgcc_s_seh-1.dll
+        libwinpthread-1.dll
+        unicorn.dll
+    )
+endfunction(copy_yuzu_unicorn_deps)
