c493bd9cc927f9119960e6dc084d374a2ec75d26
spycrab <spycrab@users.noreply.github.com>
2018-01-14 19:15:45 +0100

    Implement "About" dialog

---
+++ src/yuzu/about_dialog.h
@@ -0,0 +1,23 @@
+// Copyright 2018 yuzu Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <memory>
+#include <QDialog>
+
+namespace Ui {
+class AboutDialog;
+}
+
+class AboutDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit AboutDialog(QWidget* parent);
+    ~AboutDialog();
+
+private:
+    std::unique_ptr<Ui::AboutDialog> ui;
+};
