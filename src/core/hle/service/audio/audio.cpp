d81a984d4a52a05c9a2ef21e3e9d6742251f93ac
bunnei <bunneidev@gmail.com>
2018-01-14 21:40:37 -0500

    audio: Add files to CMake.

--- src/core/hle/service/audio/audio.cpp
+++ src/core/hle/service/audio/audio.cpp
@@ -2,7 +2,6 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include "core/hle/ipc_helpers.h"
 #include "core/hle/service/audio/audio.h"
 #include "core/hle/service/audio/audout_u.h"
 
