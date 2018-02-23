a63d4fa5b41048104e5280b86cf9a4d0b86f0f1f
shinyquagsire23 <mtinc2@gmail.com>
2018-02-23 00:34:15 -0700

    time: Add missing time:s functions, used for libnx

--- src/core/hle/service/time/time_s.cpp
+++ src/core/hle/service/time/time_s.cpp
@@ -10,6 +10,10 @@ namespace Time {
 TIME_S::TIME_S(std::shared_ptr<Module> time) : Module::Interface(std::move(time), "time:s") {
     static const FunctionInfo functions[] = {
         {0, &TIME_S::GetStandardUserSystemClock, "GetStandardUserSystemClock"},
+        {1, &TIME_S::GetStandardNetworkSystemClock, "GetStandardNetworkSystemClock"},
+        {2, &TIME_S::GetStandardSteadyClock, "GetStandardSteadyClock"},
+        {3, &TIME_S::GetTimeZoneService, "GetTimeZoneService"},
+        {4, &TIME_S::GetStandardLocalSystemClock, "GetStandardLocalSystemClock"},
     };
     RegisterHandlers(functions);
 }
