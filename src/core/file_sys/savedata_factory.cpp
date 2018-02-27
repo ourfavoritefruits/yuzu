3209cff5307ab16044ccc22e6b922545aae8215d
Subv <subv2112@gmail.com>
2018-02-27 10:23:35 -0500

    SaveData: Use the current titleid when opening the savedata archive.

--- src/core/file_sys/savedata_factory.cpp
+++ src/core/file_sys/savedata_factory.cpp
@@ -9,6 +9,7 @@
 #include "common/string_util.h"
 #include "core/file_sys/disk_filesystem.h"
 #include "core/file_sys/savedata_factory.h"
+#include "core/hle/kernel/process.h"
 
 namespace FileSys {
 
@@ -16,8 +17,8 @@ SaveData_Factory::SaveData_Factory(std::string nand_directory)
     : nand_directory(std::move(nand_directory)) {}
 
 ResultVal<std::unique_ptr<FileSystemBackend>> SaveData_Factory::Open(const Path& path) {
-    // TODO(Subv): Somehow obtain these values.
-    u64 title_id = 0;
+    u64 title_id = Kernel::g_current_process->program_id;
+    // TODO(Subv): Somehow obtain this value.
     u32 user = 0;
     std::string save_directory = Common::StringFromFormat("%ssave/%016" PRIX64 "/%08X",
                                                           nand_directory.c_str(), title_id, user);
