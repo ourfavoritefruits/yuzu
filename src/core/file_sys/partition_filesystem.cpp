de580ccdd50f349b82f2aa81f7b63cd1f0bafb00
shinyquagsire23 <mtinc2@gmail.com>
2018-04-17 09:47:11 -0600

    file_sys: Use NGLOG

--- src/core/file_sys/partition_filesystem.cpp
+++ src/core/file_sys/partition_filesystem.cpp
@@ -114,12 +114,12 @@ u64 PartitionFilesystem::GetFileSize(const std::string& name) const {
 }
 
 void PartitionFilesystem::Print() const {
-    LOG_DEBUG(Service_FS, "Magic:                  %.4s", pfs_header.magic.data());
-    LOG_DEBUG(Service_FS, "Files:                  %u", pfs_header.num_entries);
+    NGLOG_DEBUG(Service_FS, "Magic:                  {:.4}", pfs_header.magic.data());
+    NGLOG_DEBUG(Service_FS, "Files:                  {}", pfs_header.num_entries);
     for (u32 i = 0; i < pfs_header.num_entries; i++) {
-        LOG_DEBUG(Service_FS, " > File %u:              %s (0x%" PRIX64 " bytes, at 0x%" PRIX64 ")",
-                  i, pfs_entries[i].name.c_str(), pfs_entries[i].fs_entry.size,
-                  GetFileOffset(pfs_entries[i].name));
+        NGLOG_DEBUG(Service_FS, " > File {}:              {} (0x{:X} bytes, at 0x{:X})", i,
+                    pfs_entries[i].name.c_str(), pfs_entries[i].fs_entry.size,
+                    GetFileOffset(pfs_entries[i].name));
     }
 }
 } // namespace FileSys
