5c3d5d08495073178bedccbc688ac3f3eb73abe3
Michał Janiszewski <janisozaur@gmail.com>
2018-06-05 22:46:54 +0200

    Use Ninja for MSVC AppVeyor builds

--- CMakeModules/CopyYuzuUnicornDeps.cmake
+++ CMakeModules/CopyYuzuUnicornDeps.cmake
@@ -1,6 +1,6 @@
 function(copy_yuzu_unicorn_deps target_dir)
     include(WindowsCopyFiles)
-    set(DLL_DEST "${CMAKE_BINARY_DIR}/bin/$<CONFIG>/")
+    set(DLL_DEST "${CMAKE_BINARY_DIR}/bin/")
     windows_copy_files(${target_dir} ${UNICORN_DLL_DIR} ${DLL_DEST}
         libgcc_s_seh-1.dll
         libwinpthread-1.dll
