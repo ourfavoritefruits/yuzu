6b3e54621f8f13a754fe4c600f281bd79a3243b9
bunnei <bunneidev@gmail.com>
2018-07-23 18:55:24 -0400

    maxwell_to_gl: Implement Texture::WrapMode::Border.

--- src/video_core/renderer_opengl/maxwell_to_gl.h
+++ src/video_core/renderer_opengl/maxwell_to_gl.h
@@ -112,6 +112,8 @@ inline GLenum WrapMode(Tegra::Texture::WrapMode wrap_mode) {
         return GL_MIRRORED_REPEAT;
     case Tegra::Texture::WrapMode::ClampToEdge:
         return GL_CLAMP_TO_EDGE;
+    case Tegra::Texture::WrapMode::Border:
+        return GL_CLAMP_TO_BORDER;
     case Tegra::Texture::WrapMode::ClampOGL:
         // TODO(Subv): GL_CLAMP was removed as of OpenGL 3.1, to implement GL_CLAMP, we can use
         // GL_CLAMP_TO_BORDER to get the border color of the texture, and then sample the edge to
