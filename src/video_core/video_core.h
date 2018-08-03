b4e050e6c43a56cf9570f9b054c38c149c87a4e9
Lioncash <mathew1800@gmail.com>
2018-08-03 12:47:40 -0400

    video_core: Remove unimplemented Start() function prototype
    Given this has no definition, we can just remove it entirely.

--- src/video_core/video_core.h
+++ src/video_core/video_core.h
@@ -23,9 +23,6 @@ extern std::unique_ptr<RendererBase> g_renderer; ///< Renderer plugin
 // qt ui)
 extern std::atomic<bool> g_toggle_framelimit_enabled;
 
-/// Start the video core
-void Start();
-
 /// Initialize the video core
 bool Init(EmuWindow& emu_window);
 
