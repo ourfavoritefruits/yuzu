df51207ed20b72da4e39b33d5d4159328ee72a9c
Lioncash <mathew1800@gmail.com>
2018-08-04 17:39:06 -0400

    service: Remove redundant #pragma once directives
    These don't do anything within .cpp files (we don't include cpp files,
    so...)

--- src/core/hle/service/audio/audren_a.cpp
+++ src/core/hle/service/audio/audren_a.cpp
@@ -2,8 +2,6 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#pragma once
-
 #include "core/hle/service/audio/audren_a.h"
 
 namespace Service::Audio {
