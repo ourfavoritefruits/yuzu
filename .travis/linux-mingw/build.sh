a9f58593d4ac951bf15993bd280d6b2d78b3fdee
liushuyu <liushuyu011@gmail.com>
2018-08-07 22:54:57 +0800

    travis: use prebuilt image (#3839)
    * travis: use prebuilt image
    
    * travis: use prebuilt image (MinGW)

--- .travis/linux-mingw/build.sh
+++ .travis/linux-mingw/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 mkdir "$HOME/.ccache" || true
-docker run --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache ubuntu:18.04 /bin/bash -ex /yuzu/.travis/linux-mingw/docker.sh
+docker run --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache yuzuemu/build-environments:linux-mingw /bin/bash -ex /yuzu/.travis/linux-mingw/docker.sh
