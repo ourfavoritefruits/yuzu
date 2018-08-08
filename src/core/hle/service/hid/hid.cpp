3c498189b6628fdf6dadcc297a313ea51f0c4e2e
mailwl <mailwl@gmail.com>
2018-08-08 09:32:38 +0300

    hid: fix IsSixAxisSensorAtRest() response

--- src/core/hle/service/hid/hid.cpp
+++ src/core/hle/service/hid/hid.cpp
@@ -457,7 +457,7 @@ private:
     }
 
     void IsSixAxisSensorAtRest(Kernel::HLERequestContext& ctx) {
-        IPC::ResponseBuilder rb{ctx, 2};
+        IPC::ResponseBuilder rb{ctx, 3};
         rb.Push(RESULT_SUCCESS);
         // TODO (Hexagon12): Properly implement reading gyroscope values from controllers.
         rb.Push(true);
