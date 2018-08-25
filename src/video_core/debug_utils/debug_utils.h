1e6a209649909a5cfa599aef3a1971cb34f5e27d
Lioncash <mathew1800@gmail.com>
2018-08-24 20:17:47 -0400

    debug_utils: Make BreakpointObserver class' constructor explicit
    Avoids implicit conversions.

--- src/video_core/debug_utils/debug_utils.h
+++ src/video_core/debug_utils/debug_utils.h
@@ -46,7 +46,7 @@ public:
     class BreakPointObserver {
     public:
         /// Constructs the object such that it observes events of the given DebugContext.
-        BreakPointObserver(std::shared_ptr<DebugContext> debug_context)
+        explicit BreakPointObserver(std::shared_ptr<DebugContext> debug_context)
             : context_weak(debug_context) {
             std::unique_lock<std::mutex> lock(debug_context->breakpoint_mutex);
             debug_context->breakpoint_observers.push_back(this);
