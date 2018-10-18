98c7a6d62277803e1bde9dc13b5bfd74421e0f35
David Marcec <dmarcecguzman@gmail.com>
2018-10-19 01:09:34 +1100

    Used better names for mm:u and fixed bad stub
    InitializeWithId needs to return an id which is a u32 which should be a non zero value

--- src/core/hle/service/mm/mm_u.cpp
+++ src/core/hle/service/mm/mm_u.cpp
@@ -14,14 +14,14 @@ public:
     explicit MM_U() : ServiceFramework{"mm:u"} {
         // clang-format off
         static const FunctionInfo functions[] = {
-            {0, &MM_U::Initialize, "InitializeOld"},
-            {1, &MM_U::Finalize, "FinalizeOld"},
-            {2, &MM_U::SetAndWait, "SetAndWaitOld"},
-            {3, &MM_U::Get, "GetOld"},
-            {4, &MM_U::Initialize, "Initialize"},
-            {5, &MM_U::Finalize, "Finalize"},
-            {6, &MM_U::SetAndWait, "SetAndWait"},
-            {7, &MM_U::Get, "Get"},
+            {0, &MM_U::Initialize, "Initialize"},
+            {1, &MM_U::Finalize, "Finalize"},
+            {2, &MM_U::SetAndWait, "SetAndWait"},
+            {3, &MM_U::Get, "Get"},
+            {4, &MM_U::InitializeWithId, "InitializeWithId"},
+            {5, &MM_U::FinalizeWithId, "FinalizeWithId"},
+            {6, &MM_U::SetAndWaitWithId, "SetAndWaitWithId"},
+            {7, &MM_U::GetWithId, "GetWithId"},
         };
         // clang-format on
 
@@ -59,9 +59,43 @@ private:
         rb.Push(current);
     }
 
+    void InitializeWithId(Kernel::HLERequestContext& ctx) {
+        LOG_WARNING(Service_MM, "(STUBBED) called");
+        IPC::ResponseBuilder rb{ctx, 3};
+        rb.Push(RESULT_SUCCESS);
+        rb.Push<u32>(id); // Any non zero value
+    }
+
+    void FinalizeWithId(Kernel::HLERequestContext& ctx) {
+        LOG_WARNING(Service_MM, "(STUBBED) called");
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(RESULT_SUCCESS);
+    }
+
+    void SetAndWaitWithId(Kernel::HLERequestContext& ctx) {
+        IPC::RequestParser rp{ctx};
+        u32 input_id = rp.Pop<u32>();
+        min = rp.Pop<u32>();
+        max = rp.Pop<u32>();
+        current = min;
+
+        LOG_WARNING(Service_MM, "(STUBBED) called, input_id=0x{:X}, min=0x{:X}, max=0x{:X}",
+                    input_id, min, max);
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(RESULT_SUCCESS);
+    }
+
+    void GetWithId(Kernel::HLERequestContext& ctx) {
+        LOG_WARNING(Service_MM, "(STUBBED) called");
+        IPC::ResponseBuilder rb{ctx, 3};
+        rb.Push(RESULT_SUCCESS);
+        rb.Push(current);
+    }
+
     u32 min{0};
     u32 max{0};
     u32 current{0};
+    u32 id{1};
 };
 
 void InstallInterfaces(SM::ServiceManager& service_manager) {
