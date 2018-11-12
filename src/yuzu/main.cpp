9333ee29ca5958003d1ccbb4973c026d579c319c
Tobias <thm.frey@gmail.com>
2018-11-12 20:57:15 +0100

    yuzu: Add a missing "!" to fix the stuck-in-fullscreen bug

--- src/yuzu/main.cpp
+++ src/yuzu/main.cpp
@@ -1612,7 +1612,7 @@ void GMainWindow::closeEvent(QCloseEvent* event) {
         return;
     }
 
-    if (ui.action_Fullscreen->isChecked()) {
+    if (!ui.action_Fullscreen->isChecked()) {
         UISettings::values.geometry = saveGeometry();
         UISettings::values.renderwindow_geometry = render_window->saveGeometry();
     }
