1af13e0802e612c998c9d0382cfb8705c4d6591c
Lioncash <mathew1800@gmail.com>
2018-11-14 17:08:56 -0500

    profile_manager: Move UUID Format function definitions into the cpp file
    Avoids relying on fmt always being indirectly included.

--- src/core/hle/service/acc/profile_manager.cpp
+++ src/core/hle/service/acc/profile_manager.cpp
@@ -2,7 +2,10 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <cstring>
 #include <random>
+
+#include <fmt/format.h>
 
 #include "common/file_util.h"
 #include "core/hle/service/acc/profile_manager.h"
@@ -37,6 +40,19 @@ UUID UUID::Generate() {
     std::mt19937 gen(device());
     std::uniform_int_distribution<u64> distribution(1, std::numeric_limits<u64>::max());
     return UUID{distribution(gen), distribution(gen)};
+}
+
+std::string UUID::Format() const {
+    return fmt::format("0x{:016X}{:016X}", uuid[1], uuid[0]);
+}
+
+std::string UUID::FormatSwitch() const {
+    std::array<u8, 16> s{};
+    std::memcpy(s.data(), uuid.data(), sizeof(u128));
+    return fmt::format("{:02x}{:02x}{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:02x}{"
+                       ":02x}{:02x}{:02x}{:02x}{:02x}",
+                       s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11],
+                       s[12], s[13], s[14], s[15]);
 }
 
 ProfileManager::ProfileManager() {
