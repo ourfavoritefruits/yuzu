edd5b6f12f4959588dbeb02d43180af3b3f7f226
Lioncash <mathew1800@gmail.com>
2018-11-16 00:08:41 -0500

    file_sys/errors: Get rid of the ErrCodes namespace
    There's no real point to keeping the separate enum around, especially
    given the name of the error code itself is supposed to document what the
    value actually represents.

--- src/core/file_sys/errors.h
+++ src/core/file_sys/errors.h
@@ -8,19 +8,6 @@
 
 namespace FileSys {
 
-namespace ErrCodes {
-enum {
-    NotFound = 1,
-    EntityNotFound = 1002,
-    SdCardNotFound = 2001,
-    RomFSNotFound = 2520,
-    InvalidOffset = 6061,
-    InvalidSize = 6062,
-};
-}
-
-constexpr ResultCode ERROR_PATH_NOT_FOUND(ErrorModule::FS, ErrCodes::NotFound);
-
 // TODO(bunnei): Replace these with correct errors for Switch OS
 constexpr ResultCode ERROR_INVALID_PATH(-1);
 constexpr ResultCode ERROR_UNSUPPORTED_OPEN_FLAGS(-1);
@@ -31,9 +18,10 @@ constexpr ResultCode ERROR_DIRECTORY_ALREADY_EXISTS(-1);
 constexpr ResultCode ERROR_FILE_ALREADY_EXISTS(-1);
 constexpr ResultCode ERROR_DIRECTORY_NOT_EMPTY(-1);
 
-constexpr ResultCode ERROR_ENTITY_NOT_FOUND{ErrorModule::FS, ErrCodes::EntityNotFound};
-constexpr ResultCode ERROR_SD_CARD_NOT_FOUND{ErrorModule::FS, ErrCodes::SdCardNotFound};
-constexpr ResultCode ERROR_INVALID_OFFSET{ErrorModule::FS, ErrCodes::InvalidOffset};
-constexpr ResultCode ERROR_INVALID_SIZE{ErrorModule::FS, ErrCodes::InvalidSize};
+constexpr ResultCode ERROR_PATH_NOT_FOUND{ErrorModule::FS, 1};
+constexpr ResultCode ERROR_ENTITY_NOT_FOUND{ErrorModule::FS, 1002};
+constexpr ResultCode ERROR_SD_CARD_NOT_FOUND{ErrorModule::FS, 2001};
+constexpr ResultCode ERROR_INVALID_OFFSET{ErrorModule::FS, 6061};
+constexpr ResultCode ERROR_INVALID_SIZE{ErrorModule::FS, 6062};
 
 } // namespace FileSys
