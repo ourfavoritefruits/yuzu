312ef596a5fa93f93444b1485d43398f0bcde043
Zach Hilman <zachhilman@gmail.com>
2018-11-17 16:26:03 -0500

    configure_input_player: Set minimum width on controls

--- src/yuzu_cmd/config.cpp
+++ src/yuzu_cmd/config.cpp
@@ -324,13 +324,12 @@ void Config::ReadValues() {
     Settings::values.current_user = std::clamp<int>(
         sdl2_config->GetInteger("System", "current_user", 0), 0, Service::Account::MAX_USERS - 1);
 
-	const auto enabled = sdl2_config->GetBoolean("System", "rng_seed_enabled", false);
-	if (enabled) {
-		Settings::values.rng_seed = sdl2_config->GetInteger("System", "rng_seed", 0);
-	}
-	else {
-		Settings::values.rng_seed = std::nullopt;
-	}
+    const auto enabled = sdl2_config->GetBoolean("System", "rng_seed_enabled", false);
+    if (enabled) {
+        Settings::values.rng_seed = sdl2_config->GetInteger("System", "rng_seed", 0);
+    } else {
+        Settings::values.rng_seed = std::nullopt;
+    }
 
     // Core
     Settings::values.use_cpu_jit = sdl2_config->GetBoolean("Core", "use_cpu_jit", true);
