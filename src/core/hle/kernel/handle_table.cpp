ac966e4213c4a60af01fa99eec8427674a569cf0
Lioncash <mathew1800@gmail.com>
2018-12-04 01:40:16 -0500

    kernel/handle_table: Amend reference to CTR-OS in Create()
    Another hold-over from Citra.

--- src/core/hle/kernel/handle_table.cpp
+++ src/core/hle/kernel/handle_table.cpp
@@ -42,9 +42,10 @@ ResultVal<Handle> HandleTable::Create(SharedPtr<Object> obj) {
     u16 generation = next_generation++;
 
     // Overflow count so it fits in the 15 bits dedicated to the generation in the handle.
-    // CTR-OS doesn't use generation 0, so skip straight to 1.
-    if (next_generation >= (1 << 15))
+    // Horizon OS uses zero to represent an invalid handle, so skip to 1.
+    if (next_generation >= (1 << 15)) {
         next_generation = 1;
+    }
 
     generations[slot] = generation;
     objects[slot] = std::move(obj);
