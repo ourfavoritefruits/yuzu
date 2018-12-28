PabloMK7/citra

Qt/Configure: Use sidebar to divide tabs into smaller groups

src/citra_qt/configuration/configure_dialog.cpp
@@ -2,6 +2,8 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <QHash>
+#include <QListWidgetItem>
 #include "citra_qt/configuration/config.h"
 #include "citra_qt/configuration/configure_dialog.h"
 #include "citra_qt/hotkeys.h"
@@ -13,8 +15,15 @@ ConfigureDialog::ConfigureDialog(QWidget* parent, const HotkeyRegistry& registry
     ui->setupUi(this);
     ui->generalTab->PopulateHotkeyList(registry);
     this->setConfiguration();
+    this->PopulateSelectionList();
     connect(ui->generalTab, &ConfigureGeneral::languageChanged, this,
             &ConfigureDialog::onLanguageChanged);
+    connect(ui->selectorList, &QListWidget::itemSelectionChanged, this,
+            &ConfigureDialog::UpdateVisibleTabs);
+
+    adjustSize();
+
+    ui->selectorList->setCurrentRow(0);
 }
 
 ConfigureDialog::~ConfigureDialog() = default;
@@ -34,6 +43,22 @@ void ConfigureDialog::applyConfiguration() {
     Settings::LogSettings();
 }
 
+void ConfigureDialog::PopulateSelectionList() {
+
+    const std::array<std::pair<QString, QStringList>, 4> items{
+        {{tr("General"), {tr("General"), tr("Web"), tr("Debug")}},
+         {tr("System"), {tr("System"), tr("Audio")}},
+         {tr("Graphics"), {tr("Graphics")}},
+         {tr("Controls"), {tr("Input")}}}};
+
+    for (const auto& entry : items) {
+        auto* item = new QListWidgetItem(entry.first);
+        item->setData(Qt::UserRole, entry.second);
+
+        ui->selectorList->addItem(item);
+    }
+}
+
 void ConfigureDialog::onLanguageChanged(const QString& locale) {
     emit languageChanged(locale);
     ui->retranslateUi(this);
@@ -45,4 +70,23 @@ void ConfigureDialog::onLanguageChanged(const QString& locale) {
     ui->cameraTab->retranslateUi();
     ui->debugTab->retranslateUi();
     ui->webTab->retranslateUi();
+}
+
+void ConfigureDialog::UpdateVisibleTabs() {
+    auto items = ui->selectorList->selectedItems();
+    if (items.isEmpty())
+        return;
+
+    const QHash<QString, QWidget*> widgets = {
+        {tr("General"), ui->generalTab}, {tr("System"), ui->systemTab},
+        {tr("Input"), ui->inputTab},     {tr("Graphics"), ui->graphicsTab},
+        {tr("Audio"), ui->audioTab},     {tr("Camera"), ui->cameraTab},
+        {tr("Debug"), ui->debugTab},     {tr("Web"), ui->webTab}};
+
+    ui->tabWidget->clear();
+
+    QStringList tabs = items[0]->data(Qt::UserRole).toStringList();
+
+    for (const auto& tab : tabs)
+        ui->tabWidget->addTab(widgets[tab], tab);
 }

ourfavoritefruits/yuzu

src/yuzu/configuration/configure_dialog.cpp
@@ -2,6 +2,8 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <QHash>
+#include <QListWidgetItem>
 #include "core/settings.h"
 #include "ui_configure.h"
 #include "yuzu/configuration/config.h"
@@ -13,6 +15,13 @@ ConfigureDialog::ConfigureDialog(QWidget* parent, const HotkeyRegistry& registry
     ui->setupUi(this);
     ui->generalTab->PopulateHotkeyList(registry);
     this->setConfiguration();
+    this->PopulateSelectionList();
+    connect(ui->selectorList, &QListWidget::itemSelectionChanged, this,
+            &ConfigureDialog::UpdateVisibleTabs);
+
+    adjustSize();
+
+    ui->selectorList->setCurrentRow(0);
 }
 
 ConfigureDialog::~ConfigureDialog() = default;
@@ -29,4 +38,38 @@ void ConfigureDialog::applyConfiguration() {
     ui->debugTab->applyConfiguration();
     ui->webTab->applyConfiguration();
     Settings::Apply();
+}
+
+void ConfigureDialog::PopulateSelectionList() {
+    const std::array<std::pair<QString, QStringList>, 4> items{
+        {{tr("General"), {tr("General"), tr("Web"), tr("Debug"), tr("Game List")}},
+         {tr("System"), {tr("System"), tr("Audio")}},
+         {tr("Graphics"), {tr("Graphics")}},
+         {tr("Controls"), {tr("Input")}}}};
+
+    for (const auto& entry : items) {
+        auto* const item = new QListWidgetItem(entry.first);
+        item->setData(Qt::UserRole, entry.second);
+
+        ui->selectorList->addItem(item);
+    }
+}
+
+void ConfigureDialog::UpdateVisibleTabs() {
+    const auto items = ui->selectorList->selectedItems();
+    if (items.isEmpty())
+        return;
+
+    const std::map<QString, QWidget*> widgets = {
+        {tr("General"), ui->generalTab}, {tr("System"), ui->systemTab},
+        {tr("Input"), ui->inputTab},     {tr("Graphics"), ui->graphicsTab},
+        {tr("Audio"), ui->audioTab},     {tr("Debug"), ui->debugTab},
+        {tr("Web"), ui->webTab},         {tr("Game List"), ui->gameListTab}};
+
+    ui->tabWidget->clear();
+
+    const QStringList tabs = items[0]->data(Qt::UserRole).toStringList();
+
+    for (const auto& tab : tabs)
+        ui->tabWidget->addTab(widgets.find(tab)->second, tab);
 }