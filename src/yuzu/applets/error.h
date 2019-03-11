76452cd5b3bfd71ebc17ec8da4c593cdd5ee6d0e
Zach Hilman <zachhilman@gmail.com>
2019-03-11 19:39:20 -0400

    qt: Add dialog implementation of Error applet

---
+++ src/yuzu/applets/error.h
@@ -0,0 +1,33 @@
+// Copyright 2019 yuzu Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <QObject>
+
+#include "core/frontend/applets/error.h"
+
+class GMainWindow;
+
+class QtErrorDisplay final : public QObject, public Core::Frontend::ErrorApplet {
+    Q_OBJECT
+
+public:
+    explicit QtErrorDisplay(GMainWindow& parent);
+    ~QtErrorDisplay() override;
+
+    void ShowError(ResultCode error, std::function<void()> finished) const override;
+    void ShowErrorWithTimestamp(ResultCode error, std::chrono::seconds time,
+                                std::function<void()> finished) const override;
+    void ShowCustomErrorText(ResultCode error, std::string dialog_text, std::string fullscreen_text,
+                             std::function<void()> finished) const override;
+
+signals:
+    void MainWindowDisplayError(QString error) const;
+
+private:
+    void MainWindowFinishedError();
+
+    mutable std::function<void()> callback;
+};
