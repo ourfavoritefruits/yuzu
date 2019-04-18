19f8f86bdbe86486017e2fd60ecc17a799bf9a96
Lioncash <mathew1800@gmail.com>
2019-04-18 15:57:16 -0400

    service/audctl: Implement GetTargetVolumeMin() and GetTargetVolumeMax()
    These two service functions are literally hardcoded to always return
    these values without any other error checking.

--- src/core/hle/service/audio/audctl.h
+++ src/core/hle/service/audio/audctl.h
@@ -12,6 +12,10 @@ class AudCtl final : public ServiceFramework<AudCtl> {
 public:
     explicit AudCtl();
     ~AudCtl() override;
+
+private:
+    void GetTargetVolumeMin(Kernel::HLERequestContext& ctx);
+    void GetTargetVolumeMax(Kernel::HLERequestContext& ctx);
 };
 
 } // namespace Service::Audio
