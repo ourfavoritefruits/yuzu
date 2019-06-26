daf9cd9358cee49fe5a876469da529cd3660a618
Zach Hilman <zachhilman@gmail.com>
2019-06-26 18:02:36 -0400

    prepo: Implement SaveReport New and System variants

--- src/core/hle/service/prepo/prepo.cpp
+++ src/core/hle/service/prepo/prepo.cpp
@@ -2,18 +2,14 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#include <json.hpp>
-#include "common/file_util.h"
 #include "common/hex_util.h"
 #include "common/logging/log.h"
-#include "common/scm_rev.h"
 #include "core/hle/ipc_helpers.h"
 #include "core/hle/kernel/process.h"
 #include "core/hle/service/acc/profile_manager.h"
 #include "core/hle/service/prepo/prepo.h"
 #include "core/hle/service/service.h"
 #include "core/reporter.h"
-#include "core/settings.h"
 
 namespace Service::PlayReport {
 
@@ -22,14 +18,14 @@ public:
     explicit PlayReport(const char* name) : ServiceFramework{name} {
         // clang-format off
         static const FunctionInfo functions[] = {
-            {10100, nullptr, "SaveReportOld"},
-            {10101, &PlayReport::SaveReportWithUserOld, "SaveReportWithUserOld"},
-            {10102, nullptr, "SaveReport"},
-            {10103, nullptr, "SaveReportWithUser"},
+            {10100, &PlayReport::SaveReport<Core::Reporter::PlayReportType::Old>, "SaveReportOld"},
+            {10101, &PlayReport::SaveReportWithUser<Core::Reporter::PlayReportType::Old>, "SaveReportWithUserOld"},
+            {10102, &PlayReport::SaveReport<Core::Reporter::PlayReportType::New>, "SaveReport"},
+            {10103, &PlayReport::SaveReportWithUser<Core::Reporter::PlayReportType::New>, "SaveReportWithUser"},
             {10200, nullptr, "RequestImmediateTransmission"},
             {10300, nullptr, "GetTransmissionStatus"},
-            {20100, nullptr, "SaveSystemReport"},
-            {20101, nullptr, "SaveSystemReportWithUser"},
+            {20100, &PlayReport::SaveSystemReport, "SaveSystemReport"},
+            {20101, &PlayReport::SaveSystemReportWithUser, "SaveSystemReportWithUser"},
             {20200, nullptr, "SetOperationMode"},
             {30100, nullptr, "ClearStorage"},
             {30200, nullptr, "ClearStatistics"},
@@ -47,7 +43,28 @@ public:
     }
 
 private:
-    void SaveReportWithUserOld(Kernel::HLERequestContext& ctx) {
+    template <Core::Reporter::PlayReportType Type>
+    void SaveReport(Kernel::HLERequestContext& ctx) {
+        IPC::RequestParser rp{ctx};
+        const auto process_id = rp.PopRaw<u64>();
+
+        const auto data1 = ctx.ReadBuffer(0);
+        const auto data2 = ctx.ReadBuffer(1);
+
+        LOG_DEBUG(Service_PREPO,
+                  "called, type={:02X}, process_id={:016X}, data1_size={:016X}, data2_size={:016X}",
+                  static_cast<u8>(Type), process_id, data1.size(), data2.size());
+
+        const auto& reporter{Core::System::GetInstance().GetReporter()};
+        reporter.SavePlayReport(Type, Core::CurrentProcess()->GetTitleID(), {data1, data2},
+                                process_id);
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(RESULT_SUCCESS);
+    }
+
+    template <Core::Reporter::PlayReportType Type>
+    void SaveReportWithUser(Kernel::HLERequestContext& ctx) {
         IPC::RequestParser rp{ctx};
         const auto user_id = rp.PopRaw<u128>();
         const auto process_id = rp.PopRaw<u64>();
@@ -57,12 +74,51 @@ private:
 
         LOG_DEBUG(
             Service_PREPO,
-            "called, user_id={:016X}{:016X}, unk1={:016X}, data1_size={:016X}, data2_size={:016X}",
-            user_id[1], user_id[0], process_id, data1.size(), data2.size());
+            "called, type={:02X}, user_id={:016X}{:016X}, process_id={:016X}, data1_size={:016X}, "
+            "data2_size={:016X}",
+            static_cast<u8>(Type), user_id[1], user_id[0], process_id, data1.size(), data2.size());
 
         const auto& reporter{Core::System::GetInstance().GetReporter()};
-        reporter.SavePlayReport(Core::CurrentProcess()->GetTitleID(), process_id, {data1, data2},
-                                user_id);
+        reporter.SavePlayReport(Type, Core::CurrentProcess()->GetTitleID(), {data1, data2},
+                                process_id, user_id);
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(RESULT_SUCCESS);
+    }
+
+    void SaveSystemReport(Kernel::HLERequestContext& ctx) {
+        IPC::RequestParser rp{ctx};
+        const auto title_id = rp.PopRaw<u64>();
+
+        const auto data1 = ctx.ReadBuffer(0);
+        const auto data2 = ctx.ReadBuffer(1);
+
+        LOG_DEBUG(Service_PREPO, "called, title_id={:016X}, data1_size={:016X}, data2_size={:016X}",
+                  title_id, data1.size(), data2.size());
+
+        const auto& reporter{Core::System::GetInstance().GetReporter()};
+        reporter.SavePlayReport(Core::Reporter::PlayReportType::System, title_id, {data1, data2});
+
+        IPC::ResponseBuilder rb{ctx, 2};
+        rb.Push(RESULT_SUCCESS);
+    }
+
+    void SaveSystemReportWithUser(Kernel::HLERequestContext& ctx) {
+        IPC::RequestParser rp{ctx};
+        const auto user_id = rp.PopRaw<u128>();
+        const auto title_id = rp.PopRaw<u64>();
+
+        const auto data1 = ctx.ReadBuffer(0);
+        const auto data2 = ctx.ReadBuffer(1);
+
+        LOG_DEBUG(Service_PREPO,
+                  "called, user_id={:016X}{:016X}, title_id={:016X}, data1_size={:016X}, "
+                  "data2_size={:016X}",
+                  user_id[1], user_id[0], title_id, data1.size(), data2.size());
+
+        const auto& reporter{Core::System::GetInstance().GetReporter()};
+        reporter.SavePlayReport(Core::Reporter::PlayReportType::System, title_id, {data1, data2},
+                                std::nullopt, user_id);
 
         IPC::ResponseBuilder rb{ctx, 2};
         rb.Push(RESULT_SUCCESS);
