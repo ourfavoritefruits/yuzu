d45ad75404e75d2d4fbf3cc0d01d4a01cda1dabb
Zach Hilman <zachhilman@gmail.com>
2019-10-05 00:09:11 -0400

    ci: Add version counter variable

--- .ci/scripts/windows/exec.sh
+++ .ci/scripts/windows/exec.sh
@@ -2,4 +2,4 @@
 
 mkdir -p "ccache" || true
 chmod a+x ./.ci/scripts/windows/docker.sh
-docker run -e CCACHE_DIR=/yuzu/ccache -v $(pwd):/yuzu yuzuemu/build-environments:linux-mingw /bin/bash -ex /yuzu/.ci/scripts/windows/docker.sh
+docker run -e CCACHE_DIR=/yuzu/ccache -v $(pwd):/yuzu yuzuemu/build-environments:linux-mingw /bin/bash -ex /yuzu/.ci/scripts/windows/docker.sh $1
