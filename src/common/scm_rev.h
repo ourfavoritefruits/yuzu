3d4a0b94e368ddab44cbfe0180ccfa76dfb6ac81
Zach Hilman <zachhilman@gmail.com>
2019-10-05 00:09:49 -0400

    common: Add additional SCM revision fields

--- src/common/scm_rev.h
+++ src/common/scm_rev.h
@@ -13,6 +13,9 @@ extern const char g_build_name[];
 extern const char g_build_date[];
 extern const char g_build_fullname[];
 extern const char g_build_version[];
+extern const char g_build_id[];
+extern const char g_title_bar_format_idle[];
+extern const char g_title_bar_format_running[];
 extern const char g_shader_cache_version[];
 
 } // namespace Common
