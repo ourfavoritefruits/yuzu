5e553a6c267f4ab96a89833f1006ea27fd78b30a
Zach Hilman <zachhilman@gmail.com>
2019-10-23 15:23:43 -0400

    ci: Add build name to archive root folder

--- .ci/scripts/linux/upload.sh
+++ .ci/scripts/linux/upload.sh
@@ -5,10 +5,11 @@
 REV_NAME="yuzu-linux-${GITDATE}-${GITREV}"
 ARCHIVE_NAME="${REV_NAME}.tar.xz"
 COMPRESSION_FLAGS="-cJvf"
+DIR_NAME="${REV_NAME}_${RELEASE_NAME}"
 
-mkdir "$REV_NAME"
+mkdir "$DIR_NAME"
 
-cp build/bin/yuzu-cmd "$REV_NAME"
-cp build/bin/yuzu "$REV_NAME"
+cp build/bin/yuzu-cmd "$DIR_NAME"
+cp build/bin/yuzu "$DIR_NAME"
 
 . .ci/scripts/common/post-upload.sh
