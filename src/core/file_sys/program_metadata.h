23878bf360b95561729ee1208118cb8e13b6aeca
Lioncash <mathew1800@gmail.com>
2019-11-12 05:20:26 -0500

    loader; Resolve sign conversion/truncation errors

--- src/core/file_sys/program_metadata.h
+++ src/core/file_sys/program_metadata.h
@@ -47,8 +47,8 @@ public:
     Loader::ResultStatus Load(VirtualFile file);
 
     // Load from parameters instead of NPDM file, used for KIP
-    void LoadManual(bool is_64_bit, ProgramAddressSpaceType address_space, u8 main_thread_prio,
-                    u8 main_thread_core, u32 main_thread_stack_size, u64 title_id,
+    void LoadManual(bool is_64_bit, ProgramAddressSpaceType address_space, s32 main_thread_prio,
+                    u32 main_thread_core, u32 main_thread_stack_size, u64 title_id,
                     u64 filesystem_permissions, KernelCapabilityDescriptors capabilities);
 
     bool Is64BitProgram() const;
