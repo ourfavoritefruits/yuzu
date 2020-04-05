ea5ee9918e0e678c76f0ee1e791269c8dc7f05f4
bunnei <bunneidev@gmail.com>
2020-04-05 14:44:26 -0400

    kernel: memory: Add memory_types.h, for things that are commonly used in memory code.

---
+++ src/core/hle/kernel/memory/memory_types.h
@@ -0,0 +1,18 @@
+// Copyright 2020 yuzu Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <array>
+
+#include "common/common_types.h"
+
+namespace Kernel::Memory {
+
+constexpr std::size_t PageBits{12};
+constexpr std::size_t PageSize{1 << PageBits};
+
+using Page = std::array<u8, PageSize>;
+
+} // namespace Kernel::Memory
