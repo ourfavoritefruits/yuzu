67b3df683b91ca7f207c0ead898ee09bd5c4ec58
bunnei <bunneidev@gmail.com>
2020-04-08 21:09:16 -0400

    service: time: Update for new shared memory layout.

--- src/core/hle/service/time/time_sharedmemory.cpp
+++ src/core/hle/service/time/time_sharedmemory.cpp
@@ -6,6 +6,7 @@
 #include "core/core_timing.h"
 #include "core/core_timing_util.h"
 #include "core/hardware_properties.h"
+#include "core/hle/kernel/kernel.h"
 #include "core/hle/service/time/clock_types.h"
 #include "core/hle/service/time/steady_clock_core.h"
 #include "core/hle/service/time/time_sharedmemory.h"
@@ -15,9 +16,7 @@ namespace Service::Time {
 static constexpr std::size_t SHARED_MEMORY_SIZE{0x1000};
 
 SharedMemory::SharedMemory(Core::System& system) : system(system) {
-    shared_memory_holder = Kernel::SharedMemory::Create(
-        system.Kernel(), nullptr, SHARED_MEMORY_SIZE, Kernel::MemoryPermission::ReadWrite,
-        Kernel::MemoryPermission::Read, 0, Kernel::MemoryRegion::BASE, "Time:SharedMemory");
+    shared_memory_holder = SharedFrom(&system.Kernel().GetTimeSharedMem());
     std::memset(shared_memory_holder->GetPointer(), 0, SHARED_MEMORY_SIZE);
 }
 
