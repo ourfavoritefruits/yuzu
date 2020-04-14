551c61bf2781250d2817a7429a7a6c4b98ed7dc4
bunnei <bunneidev@gmail.com>
2020-04-14 15:08:13 -0400

    yuzu: game_list: Fix 'Open Save Data Location' for device saves.

--- src/yuzu/game_list.h
+++ src/yuzu/game_list.h
@@ -73,7 +73,7 @@ public:
 signals:
     void GameChosen(QString game_path);
     void ShouldCancelWorker();
-    void OpenFolderRequested(u64 program_id, GameListOpenTarget target);
+    void OpenFolderRequested(GameListOpenTarget target, const std::string& game_path);
     void OpenTransferableShaderCacheRequested(u64 program_id);
     void DumpRomFSRequested(u64 program_id, const std::string& game_path);
     void CopyTIDRequested(u64 program_id);
