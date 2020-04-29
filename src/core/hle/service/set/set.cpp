b4dbf1b9c7a69ee7ba9682fe0d6c8377f0512a34
David Marcec <dmarcecguzman@gmail.com>
2020-04-29 11:15:21 +1000

    Don't fail silently for vi, sm, set and ns services

--- src/core/hle/service/set/set.cpp
+++ src/core/hle/service/set/set.cpp
@@ -67,6 +67,7 @@ void SET::MakeLanguageCode(Kernel::HLERequestContext& ctx) {
     const auto index = rp.Pop<u32>();
 
     if (index >= available_language_codes.size()) {
+        LOG_ERROR(Service_SET, "Invalid language code index! index={}", index);
         IPC::ResponseBuilder rb{ctx, 2};
         rb.Push(ERR_INVALID_LANGUAGE);
         return;
