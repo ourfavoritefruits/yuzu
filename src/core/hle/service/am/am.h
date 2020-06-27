ca25a3845eed214ac3bd76d37c47739dfbd56865
VolcaEM <63682805+VolcaEM@users.noreply.github.com>
2020-06-27 02:32:26 +0200

    am: Update function tables and add missing classes (#4169)
    * am: Update function tables and add missing classes
    
    * Remove comments (1/5)
    
    * Remove comments (2/5)
    
    * Remove comments (3/5)
    
    * Remove comments (4/5)
    
    * Remove comments (5/5)
    
    * Remove unused classes (1/2)
    
    * Remove unused classes (2/2)

--- src/core/hle/service/am/am.h
+++ src/core/hle/service/am/am.h
@@ -138,7 +138,7 @@ private:
     void SetFocusHandlingMode(Kernel::HLERequestContext& ctx);
     void SetRestartMessageEnabled(Kernel::HLERequestContext& ctx);
     void SetOutOfFocusSuspendingEnabled(Kernel::HLERequestContext& ctx);
-    void SetScreenShotImageOrientation(Kernel::HLERequestContext& ctx);
+    void SetAlbumImageOrientation(Kernel::HLERequestContext& ctx);
     void CreateManagedDisplayLayer(Kernel::HLERequestContext& ctx);
     void SetHandlesRequestToDisplay(Kernel::HLERequestContext& ctx);
     void SetIdleTimeDetectionExtension(Kernel::HLERequestContext& ctx);
--- src/core/hle/service/am/spsm.cpp
+++ src/core/hle/service/am/spsm.cpp
@@ -10,17 +10,17 @@ SPSM::SPSM() : ServiceFramework{"spsm"} {
     // clang-format off
     static const FunctionInfo functions[] = {
         {0, nullptr, "GetState"},
-        {1, nullptr, "SleepSystemAndWaitAwake"},
-        {2, nullptr, "Unknown1"},
-        {3, nullptr, "Unknown2"},
+        {1, nullptr, "EnterSleep"},
+        {2, nullptr, "GetLastWakeReason"},
+        {3, nullptr, "Shutdown"},
         {4, nullptr, "GetNotificationMessageEventHandle"},
-        {5, nullptr, "Unknown3"},
-        {6, nullptr, "Unknown4"},
-        {7, nullptr, "Unknown5"},
+        {5, nullptr, "ReceiveNotificationMessage"},
+        {6, nullptr, "AnalyzeLogForLastSleepWakeSequence"},
+        {7, nullptr, "ResetEventLog"},
         {8, nullptr, "AnalyzePerformanceLogForLastSleepWakeSequence"},
         {9, nullptr, "ChangeHomeButtonLongPressingTime"},
-        {10, nullptr, "Unknown6"},
-        {11, nullptr, "Unknown7"},
+        {10, nullptr, "PutErrorState"},
+        {11, nullptr, "InvalidateCurrentHomeButtonPressing"},
     };
     // clang-format on
 
