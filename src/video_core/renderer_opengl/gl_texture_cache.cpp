990b14f181d133641173498e65c9ddccc9ab16ad
ReinUsesLisp <reinuseslisp@airmail.cc>
2020-06-30 05:01:38 -0300

    video_core: Fix B5G6R5U

--- src/video_core/renderer_opengl/gl_texture_cache.cpp
+++ src/video_core/renderer_opengl/gl_texture_cache.cpp
@@ -45,7 +45,7 @@ constexpr std::array<FormatTuple, VideoCore::Surface::MaxPixelFormat> tex_format
     {GL_RGBA8_SNORM, GL_RGBA, GL_BYTE},                               // ABGR8S
     {GL_RGBA8I, GL_RGBA_INTEGER, GL_BYTE},                            // ABGR8I
     {GL_RGBA8UI, GL_RGBA_INTEGER, GL_UNSIGNED_BYTE},                  // ABGR8UI
-    {GL_RGB565, GL_RGB, GL_UNSIGNED_SHORT_5_6_5_REV},                 // B5G6R5U
+    {GL_RGB565, GL_RGB, GL_UNSIGNED_SHORT_5_6_5},                     // B5G6R5U
     {GL_RGB10_A2, GL_RGBA, GL_UNSIGNED_INT_2_10_10_10_REV},           // A2B10G10R10U
     {GL_RGB10_A2UI, GL_RGBA_INTEGER, GL_UNSIGNED_INT_2_10_10_10_REV}, // A2B10G10R10UI
     {GL_RGB5_A1, GL_RGBA, GL_UNSIGNED_SHORT_1_5_5_5_REV},             // A1B5G5R5U
