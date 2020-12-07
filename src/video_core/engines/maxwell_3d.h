bcc5c4403ab8a42adcf65f98f70a4f6d0ca94a02
Lioncash <mathew1800@gmail.com>
2020-12-06 20:46:53 -0500

    maxwell_3d: Remove unused dirty_pointer array
    This is unused and removing it shrinks the structure by 3584 bytes.

--- src/video_core/engines/maxwell_3d.h
+++ src/video_core/engines/maxwell_3d.h
@@ -1504,8 +1504,6 @@ private:
 
     bool execute_on{true};
 
-    std::array<u8, Regs::NUM_REGS> dirty_pointers{};
-
     /// Retrieves information about a specific TIC entry from the TIC buffer.
     Texture::TICEntry GetTICEntry(u32 tic_index) const;
 
