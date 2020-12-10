3802474483cf364281fdcdfbde0c83bffb0e39dc
lat9nq <22451773+lat9nq@users.noreply.github.com>
2020-12-10 03:34:16 -0500

    travis/linux: Use correct ccache directory
    Changes the bound ccache directory to `/home/yuzu/.ccache` instead of
    `/root/.ccache`, since the `/root` directory is not accessible by the
    `yuzu` user in the guest container.

--- .travis/linux/build.sh
+++ .travis/linux/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
 mkdir -p "$HOME/.ccache"
-docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/root/.ccache yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.travis/linux/docker.sh
+docker run -e ENABLE_COMPATIBILITY_REPORTING --env-file .travis/common/travis-ci.env -v $(pwd):/yuzu -v "$HOME/.ccache":/home/yuzu/.ccache yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.travis/linux/docker.sh
