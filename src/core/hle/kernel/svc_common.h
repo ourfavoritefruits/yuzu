1212fa60b68a4b92fe7715f1bc87c42bc09cba8c
bunnei <bunneidev@gmail.com>
2020-12-28 22:42:16 -0800

    core: hle: kernel: Begin moving common SVC defintions to its own header.

---
+++ src/core/hle/kernel/svc_common.h
@@ -0,0 +1,13 @@
+// Copyright 2020 yuzu emulator team
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "common/common_types.h"
+
+namespace Kernel::Svc {
+
+constexpr u32 HandleWaitMask{1u << 30};
+
+} // namespace Kernel::Svc
