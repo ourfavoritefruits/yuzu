beb951770a920fce5d993c024402146aa0c92c34
FearlessTobi <thm.frey@gmail.com>
2021-01-04 04:36:50 +0100

    Address review comments

--- src/common/logging/backend.cpp
+++ src/common/logging/backend.cpp
@@ -146,16 +146,16 @@ void ColorConsoleBackend::Write(const Entry& entry) {
 }
 
 FileBackend::FileBackend(const std::string& filename) : bytes_written(0) {
-    if (FileUtil::Exists(filename + ".old.txt")) {
-        FileUtil::Delete(filename + ".old.txt");
+    if (Common::FS::Exists(filename + ".old.txt")) {
+        Common::FS::Delete(filename + ".old.txt");
     }
-    if (FileUtil::Exists(filename)) {
-        FileUtil::Rename(filename, filename + ".old.txt");
+    if (Common::FS::Exists(filename)) {
+        Common::FS::Rename(filename, filename + ".old.txt");
     }
 
     // _SH_DENYWR allows read only access to the file for other programs.
     // It is #defined to 0 on other platforms
-    file = FileUtil::IOFile(filename, "w", _SH_DENYWR);
+    file = Common::FS::IOFile(filename, "w", _SH_DENYWR);
 }
 
 void FileBackend::Write(const Entry& entry) {
