18175c71edf90eeb73c6f47745b2bad32e4a1478
bunnei <bunneidev@gmail.com>
2021-01-31 16:54:10 -0800

    common: scope_exit: Add a cancellable ScopeExit macro.

--- src/common/scope_exit.h
+++ src/common/scope_exit.h
@@ -49,3 +49,9 @@ ScopeExitHelper<Func> ScopeExit(Func&& func) {
  * \endcode
  */
 #define SCOPE_EXIT(body) auto CONCAT2(scope_exit_helper_, __LINE__) = detail::ScopeExit([&]() body)
+
+/**
+ * This macro is similar to SCOPE_EXIT, except the object is caller managed. This is intended to be
+ * used when the caller might want to cancel the ScopeExit.
+ */
+#define SCOPE_GUARD(body) detail::ScopeExit([&]() body)
