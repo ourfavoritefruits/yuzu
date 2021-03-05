47af34003b97a27ee8456cedb367b41f8687b517
bunnei <bunneidev@gmail.com>
2021-03-05 00:13:29 -0800

    hle: kernel: KThread: Rework dummy threads & fix memory leak.
    - Dummy threads are created on thread local storage for all host threads.
    - Fixes a leak by removing creation of fibers, which are not applicable here.

--- src/core/hle/kernel/process.cpp
+++ src/core/hle/kernel/process.cpp
@@ -40,8 +40,9 @@ namespace {
 void SetupMainThread(Core::System& system, Process& owner_process, u32 priority, VAddr stack_top) {
     const VAddr entry_point = owner_process.PageTable().GetCodeRegionStart();
     ASSERT(owner_process.GetResourceLimit()->Reserve(LimitableResource::Threads, 1));
-    auto thread_res = KThread::Create(system, ThreadType::User, "main", entry_point, priority, 0,
-                                      owner_process.GetIdealCoreId(), stack_top, &owner_process);
+    auto thread_res =
+        KThread::CreateUserThread(system, ThreadType::User, "main", entry_point, priority, 0,
+                                  owner_process.GetIdealCoreId(), stack_top, &owner_process);
 
     std::shared_ptr<KThread> thread = std::move(thread_res).Unwrap();
 
