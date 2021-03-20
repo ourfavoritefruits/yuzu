01f04fee322ae2750171bb0b526054170f61ad2d
bunnei <bunneidev@gmail.com>
2021-03-19 23:29:29 -0700

    hle: kernel: k_address_space_info: Cleanup.

--- src/core/hle/kernel/k_address_space_info.cpp
+++ src/core/hle/kernel/k_address_space_info.cpp
@@ -12,15 +12,15 @@ namespace Kernel {
 namespace {
 
 enum : u64 {
-    Size_1_MB = 0x100000,
-    Size_2_MB = 2 * Size_1_MB,
-    Size_128_MB = 128 * Size_1_MB,
-    Size_1_GB = 0x40000000,
-    Size_2_GB = 2 * Size_1_GB,
-    Size_4_GB = 4 * Size_1_GB,
-    Size_6_GB = 6 * Size_1_GB,
-    Size_64_GB = 64 * Size_1_GB,
-    Size_512_GB = 512 * Size_1_GB,
+    Size_1_MB = 0x100000ULL,
+    Size_2_MB = 2ULL * Size_1_MB,
+    Size_128_MB = 128ULL * Size_1_MB,
+    Size_1_GB = 0x40000000ULL,
+    Size_2_GB = 2ULL * Size_1_GB,
+    Size_4_GB = 4ULL * Size_1_GB,
+    Size_6_GB = 6ULL * Size_1_GB,
+    Size_64_GB = 64ULL * Size_1_GB,
+    Size_512_GB = 512ULL * Size_1_GB,
     Invalid = std::numeric_limits<u64>::max(),
 };
 
