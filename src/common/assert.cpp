9be819faaf8c1e6882c4ede339f4216d7ea72059
Markus Wick <markus@selfnet.de>
2021-04-02 08:43:26 +0200

    common: Move assert failure handling into a cpp file.
    Advantage: Altering the handler does not need a full recompilation.
    Disadvantage: noreturn is droped, so the caller is a bit slower.
    
    We quite often run yuzu with a YOLO assertion handler. In fact, only very few
    games run at all with asserts. This patch allows developers to patch the handler
    without recompiling everything. The overhead of the missing "noreturn" attribute
    shoul be negletable.

---
+++ src/common/assert.cpp
@@ -0,0 +1,11 @@
+// Copyright 2021 yuzu Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include "common/assert.h"
+
+#include "common/common_funcs.h"
+
+void assert_handle_failure() {
+    Crash();
+}
