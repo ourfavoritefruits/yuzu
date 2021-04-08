6219da627b4728efacb1b6844b67aaf36bafc683
german77 <juangerman-13@hotmail.com>
2021-04-07 19:41:14 -0600

    audrec_u: Use proper names, update to 12.x

--- src/core/hle/service/audio/audrec_u.cpp
+++ src/core/hle/service/audio/audrec_u.cpp
@@ -13,16 +13,17 @@ public:
         // clang-format off
         static const FunctionInfo functions[] = {
             {0, nullptr, "GetFinalOutputRecorderState"},
-            {1, nullptr, "StartFinalOutputRecorder"},
-            {2, nullptr, "StopFinalOutputRecorder"},
+            {1, nullptr, "Start"},
+            {2, nullptr, "Stop"},
             {3, nullptr, "AppendFinalOutputRecorderBuffer"},
             {4, nullptr, "RegisterBufferEvent"},
-            {5, nullptr, "GetReleasedFinalOutputRecorderBuffer"},
+            {5, nullptr, "GetReleasedFinalOutputRecorderBuffers"},
             {6, nullptr, "ContainsFinalOutputRecorderBuffer"},
             {7, nullptr, "GetFinalOutputRecorderBufferEndTime"},
             {8, nullptr, "AppendFinalOutputRecorderBufferAuto"},
             {9, nullptr, "GetReleasedFinalOutputRecorderBufferAuto"},
             {10, nullptr, "FlushFinalOutputRecorderBuffers"},
+            {11, nullptr, "AttachWorkBuffer"},
         };
         // clang-format on
 
