6eeb532c96e8148eed665602fc0f32fbe5ce0ccc
lat9nq <22451773+lat9nq@users.noreply.github.com>
2021-06-09 17:16:29 -0400

    ci: common: Remove 7z packaging
    Removes the 7z from being package during CI, as only .tar.xz preserves
    information needed on Linux, and otherwise is just extremely redundant
    to package in addition to the .tar.xz.  This affects Linux releases and
    PR-verify artifacts only. MSVC releases do not use this script to my
    knowledge.

--- .ci/scripts/common/post-upload.sh
+++ .ci/scripts/common/post-upload.sh
@@ -9,11 +9,5 @@ cp "${REV_NAME}-source.tar.xz" "$DIR_NAME"
 
 tar $COMPRESSION_FLAGS "$ARCHIVE_NAME" "$DIR_NAME"
 
-mv "$DIR_NAME" $RELEASE_NAME
-mv "${REV_NAME}-source.tar.xz" $RELEASE_NAME
-
-7z a "$REV_NAME.7z" $RELEASE_NAME
-
 # move the compiled archive into the artifacts directory to be uploaded by travis releases
 mv "$ARCHIVE_NAME" "${ARTIFACTS_DIR}/"
-mv "$REV_NAME.7z" "${ARTIFACTS_DIR}/"
