b9b28c0457e81d80fc51670b88692a062ea9e08e
Morph <39850852+Morph1984@users.noreply.github.com>
2021-11-29 16:16:05 -0500

    core: hid: Cleanup and amend documentation

--- src/core/hid/input_converter.h
+++ src/core/hid/input_converter.h
@@ -21,7 +21,7 @@ namespace Core::HID {
 /**
  * Converts raw input data into a valid battery status.
  *
- * @param Supported callbacks: Analog, Battery, Trigger.
+ * @param callback Supported callbacks: Analog, Battery, Trigger.
  * @return A valid BatteryStatus object.
  */
 Common::Input::BatteryStatus TransformToBattery(const Common::Input::CallbackStatus& callback);
@@ -29,7 +29,7 @@ Common::Input::BatteryStatus TransformToBattery(const Common::Input::CallbackSta
 /**
  * Converts raw input data into a valid button status. Applies invert properties to the output.
  *
- * @param Supported callbacks: Analog, Button, Trigger.
+ * @param callback Supported callbacks: Analog, Button, Trigger.
  * @return A valid TouchStatus object.
  */
 Common::Input::ButtonStatus TransformToButton(const Common::Input::CallbackStatus& callback);
@@ -37,7 +37,7 @@ Common::Input::ButtonStatus TransformToButton(const Common::Input::CallbackStatu
 /**
  * Converts raw input data into a valid motion status.
  *
- * @param Supported callbacks: Motion.
+ * @param callback Supported callbacks: Motion.
  * @return A valid TouchStatus object.
  */
 Common::Input::MotionStatus TransformToMotion(const Common::Input::CallbackStatus& callback);
@@ -46,7 +46,7 @@ Common::Input::MotionStatus TransformToMotion(const Common::Input::CallbackStatu
  * Converts raw input data into a valid stick status. Applies offset, deadzone, range and invert
  * properties to the output.
  *
- * @param Supported callbacks: Stick.
+ * @param callback Supported callbacks: Stick.
  * @return A valid StickStatus object.
  */
 Common::Input::StickStatus TransformToStick(const Common::Input::CallbackStatus& callback);
@@ -54,7 +54,7 @@ Common::Input::StickStatus TransformToStick(const Common::Input::CallbackStatus&
 /**
  * Converts raw input data into a valid touch status.
  *
- * @param Supported callbacks: Touch.
+ * @param callback Supported callbacks: Touch.
  * @return A valid TouchStatus object.
  */
 Common::Input::TouchStatus TransformToTouch(const Common::Input::CallbackStatus& callback);
@@ -63,7 +63,7 @@ Common::Input::TouchStatus TransformToTouch(const Common::Input::CallbackStatus&
  * Converts raw input data into a valid trigger status. Applies offset, deadzone, range and
  * invert properties to the output. Button status uses the threshold property if necessary.
  *
- * @param Supported callbacks: Analog, Button, Trigger.
+ * @param callback Supported callbacks: Analog, Button, Trigger.
  * @return A valid TriggerStatus object.
  */
 Common::Input::TriggerStatus TransformToTrigger(const Common::Input::CallbackStatus& callback);
@@ -72,22 +72,23 @@ Common::Input::TriggerStatus TransformToTrigger(const Common::Input::CallbackSta
  * Converts raw input data into a valid analog status. Applies offset, deadzone, range and
  * invert properties to the output.
  *
- * @param Supported callbacks: Analog.
+ * @param callback Supported callbacks: Analog.
  * @return A valid AnalogStatus object.
  */
 Common::Input::AnalogStatus TransformToAnalog(const Common::Input::CallbackStatus& callback);
 
 /**
  * Converts raw analog data into a valid analog value
- * @param An analog object containing raw data and properties, bool that determines if the value
- * needs to be clamped between -1.0f and 1.0f.
+ * @param analog An analog object containing raw data and properties
+ * @param clamp_value determines if the value needs to be clamped between -1.0f and 1.0f.
  */
 void SanitizeAnalog(Common::Input::AnalogStatus& analog, bool clamp_value);
 
 /**
  * Converts raw stick data into a valid stick value
- * @param Two analog objects containing raw data and properties, bool that determines if the value
- * needs to be clamped into the unit circle.
+ * @param analog_x raw analog data and properties for the x-axis
+ * @param analog_y raw analog data and properties for the y-axis
+ * @param clamp_value bool that determines if the value needs to be clamped into the unit circle.
  */
 void SanitizeStick(Common::Input::AnalogStatus& analog_x, Common::Input::AnalogStatus& analog_y,
                    bool clamp_value);
