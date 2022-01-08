099dd0c0d299d0f3541221928814e9001a69623a
liushuyu <liushuyu011@gmail.com>
2022-01-08 16:02:49 -0700

    logging/log: use `underlying_type` instead of hardcoding types

--- src/common/logging/log.h
+++ src/common/logging/log.h
@@ -16,10 +16,12 @@
 // a generic formatter for enum classes (<= 32 bits)
 #if FMT_VERSION >= 80100
 template <typename T>
-struct fmt::formatter<T, std::enable_if_t<std::is_enum_v<T>, char>> : formatter<u32> {
+struct fmt::formatter<T, std::enable_if_t<std::is_enum_v<T>, char>>
+    : formatter<std::underlying_type_t<T>> {
     template <typename FormatContext>
     auto format(const T& value, FormatContext& ctx) -> decltype(ctx.out()) {
-        return fmt::formatter<u32>::format(static_cast<u32>(value), ctx);
+        return fmt::formatter<std::underlying_type_t<T>>::format(
+            static_cast<std::underlying_type_t<T>>(value), ctx);
     }
 };
 #endif
