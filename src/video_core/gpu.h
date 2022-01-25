d8486a99687ffcda9a9b7e3fc5691d322b3bfeab
Lioncash <mathew1800@gmail.com>
2022-01-25 13:05:37 -0500

    gpu: Tidy up forward declarations
    Over time a few forward declarations became unnecessary, so we can
    remove these to tidy up the header a little bit.

--- src/video_core/gpu.h
+++ src/video_core/gpu.h
@@ -12,9 +12,6 @@
 #include "video_core/framebuffer_config.h"
 
 namespace Core {
-namespace Frontend {
-class EmuWindow;
-}
 class System;
 } // namespace Core
 
@@ -25,7 +22,6 @@ class ShaderNotify;
 
 namespace Tegra {
 class DmaPusher;
-class CDmaPusher;
 struct CommandList;
 
 enum class RenderTargetFormat : u32 {
@@ -88,15 +84,9 @@ enum class DepthFormat : u32 {
     D32_FLOAT_S8X24_UINT = 0x19,
 };
 
-struct CommandListHeader;
-class DebugContext;
-
 namespace Engines {
-class Fermi2D;
 class Maxwell3D;
-class MaxwellDMA;
 class KeplerCompute;
-class KeplerMemory;
 } // namespace Engines
 
 enum class EngineID {
