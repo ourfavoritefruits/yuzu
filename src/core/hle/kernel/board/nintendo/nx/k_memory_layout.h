a6496deeed4e78352bdd5eb83d91bb7f41854ea4
bunnei <bunneidev@gmail.com>
2022-02-21 17:38:11 -0800

    hle: kernel: board: nx: Add k_memory_layout.h header.

---
+++ src/core/hle/kernel/board/nintendo/nx/k_memory_layout.h
@@ -0,0 +1,13 @@
+// Copyright 2022 yuzu Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include "common/common_types.h"
+
+namespace Kernel {
+
+constexpr inline PAddr MainMemoryAddress = 0x80000000;
+
+} // namespace Kernel
