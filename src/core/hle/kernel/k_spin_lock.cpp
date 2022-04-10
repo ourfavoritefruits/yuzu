ae38b8bf5eb8baed79b70974fb93705db6495807
bunnei <bunneidev@gmail.com>
2022-04-10 16:51:42 -0700

    hle: kernel: k_spin_lock: Remove unused ThreadPause.

--- src/core/hle/kernel/k_spin_lock.cpp
+++ src/core/hle/kernel/k_spin_lock.cpp
@@ -4,34 +4,6 @@
 
 #include "core/hle/kernel/k_spin_lock.h"
 
-#if _MSC_VER
-#include <intrin.h>
-#if _M_AMD64
-#define __x86_64__ 1
-#endif
-#if _M_ARM64
-#define __aarch64__ 1
-#endif
-#else
-#if __x86_64__
-#include <xmmintrin.h>
-#endif
-#endif
-
-namespace {
-
-void ThreadPause() {
-#if __x86_64__
-    _mm_pause();
-#elif __aarch64__ && _MSC_VER
-    __yield();
-#elif __aarch64__
-    asm("yield");
-#endif
-}
-
-} // namespace
-
 namespace Kernel {
 
 void KSpinLock::Lock() {
