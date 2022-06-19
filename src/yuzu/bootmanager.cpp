3ac4f3a2521e421a625fe1d7dcba05b8441fe056
german77 <juangerman-13@hotmail.com>
2022-06-19 15:54:21 -0500

    service: irs: Implement clustering processor

--- src/yuzu/bootmanager.cpp
+++ src/yuzu/bootmanager.cpp
@@ -838,7 +838,7 @@ void GRenderWindow::InitializeCamera() {
     camera_timer = std::make_unique<QTimer>();
     connect(camera_timer.get(), &QTimer::timeout, [this] { RequestCameraCapture(); });
     // This timer should be dependent of camera resolution 5ms for every 100 pixels
-    camera_timer->start(100);
+    camera_timer->start(50);
 }
 
 void GRenderWindow::FinalizeCamera() {
