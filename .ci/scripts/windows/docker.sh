caef92a584271eaec7377d5f4cea008db8a91468
lat9nq <lat9nq@gmail.com>
2022-07-05 22:32:12 -0400

    ci/windows: Copy what of FFmpeg not already present
    Prevents overwriting libwinpthreads.dll when one should already be
    present from the first DLL search.

--- .ci/scripts/windows/docker.sh
+++ .ci/scripts/windows/docker.sh
@@ -65,7 +65,7 @@ python3 .ci/scripts/windows/scan_dll.py package/*.exe package/imageformats/*.dll
 # copy FFmpeg libraries
 EXTERNALS_PATH="$(pwd)/build/externals"
 FFMPEG_DLL_PATH="$(find "${EXTERNALS_PATH}" -maxdepth 1 -type d | grep 'ffmpeg-')/bin"
-find ${FFMPEG_DLL_PATH} -type f -regex ".*\.dll" -exec cp -v {} package/ ';'
+find ${FFMPEG_DLL_PATH} -type f -regex ".*\.dll" -exec cp -nv {} package/ ';'
 
 # copy libraries from yuzu.exe path
 find "$(pwd)/build/bin/" -type f -regex ".*\.dll" -exec cp -v {} package/ ';'
