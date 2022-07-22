dc45147736136f5f3c8b63cbeb0dda5e8df19dc5
Kyle K <190571+Docteh@users.noreply.github.com>
2022-07-22 02:36:46 -0700

    ci: pass environment variables to linux docker (AppImage)
    Variables in question:
    AZURECIREPO TITLEBARFORMATIDLE TITLEBARFORMATRUNNING DISPLAYVERSION
    
    CMakeModules/GenerateSCMRev.cmake has some logic that looks at BUILD_REPOSITORY variable inside CMake
    
    src/common/CMakeLists.txt has some logic that takes some items from environment variables and
     sets variables inside CMake
    
    This is the whole section at the moment.
    
        if (DEFINED ENV{AZURECIREPO})
          set(BUILD_REPOSITORY $ENV{AZURECIREPO})
        endif()
        if (DEFINED ENV{TITLEBARFORMATIDLE})
          set(TITLE_BAR_FORMAT_IDLE $ENV{TITLEBARFORMATIDLE})
        endif ()
        if (DEFINED ENV{TITLEBARFORMATRUNNING})
          set(TITLE_BAR_FORMAT_RUNNING $ENV{TITLEBARFORMATRUNNING})
        endif ()
        if (DEFINED ENV{DISPLAYVERSION})
          set(DISPLAY_VERSION $ENV{DISPLAYVERSION})
        endif ()

--- .ci/scripts/linux/exec.sh
+++ .ci/scripts/linux/exec.sh
@@ -4,5 +4,10 @@ mkdir -p "ccache"  || true
 chmod a+x ./.ci/scripts/linux/docker.sh
 # the UID for the container yuzu user is 1027
 sudo chown -R 1027 ./
-docker run -e ENABLE_COMPATIBILITY_REPORTING -e CCACHE_DIR=/yuzu/ccache -v "$(pwd):/yuzu" -w /yuzu yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.ci/scripts/linux/docker.sh "$1"
+
+# The environment variables listed below:
+# AZURECIREPO TITLEBARFORMATIDLE TITLEBARFORMATRUNNING DISPLAYVERSION 
+#  are requested in src/common/CMakeLists.txt and appear to be provided somewhere in Azure DevOps
+
+docker run -e AZURECIREPO -e TITLEBARFORMATIDLE -e TITLEBARFORMATRUNNING -e DISPLAYVERSION -e ENABLE_COMPATIBILITY_REPORTING -e CCACHE_DIR=/yuzu/ccache -v "$(pwd):/yuzu" -w /yuzu yuzuemu/build-environments:linux-fresh /bin/bash /yuzu/.ci/scripts/linux/docker.sh "$1"
 sudo chown -R $UID ./
