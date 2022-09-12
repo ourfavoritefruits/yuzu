1deecc6f7017bb749b12fdf0914d343b1687a0a4
Kelebek1 <eeeedddccc@hotmail.co.uk>
2022-09-12 19:27:11 +0100

    Remove a pragma once from a cpp file

--- src/audio_core/sink/sink_stream.cpp
+++ src/audio_core/sink/sink_stream.cpp
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2018 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#pragma once
-
 #include <array>
 #include <atomic>
 #include <memory>
