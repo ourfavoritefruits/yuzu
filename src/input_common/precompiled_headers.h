37bc5118ea8f5891741faba8963ce9ae5f88e946
ameerj <52414509+ameerj@users.noreply.github.com>
2022-11-22 18:38:23 -0500

    CMake: Use precompiled headers

---
+++ src/input_common/precompiled_headers.h
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2022 yuzu Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <memory>
+
+#include <fmt/format.h>
+
+#include "common/assert.h"
---
+++ src/shader_recompiler/precompiled_headers.h
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2022 yuzu Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <memory>
+
+#include <fmt/format.h>
+
+#include "common/assert.h"
+#include "frontend/maxwell/translate/impl/impl.h"
