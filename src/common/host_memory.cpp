4d7be85e730bc7f46ec5433c425733c4b65a90ed
Merry <git@mary.rs>
2023-01-01 11:14:44 +0000

    host_memory: Allocate virtual_base with MAP_NORESERVE
    Specify that we do not require swap to be reserved for this address range;
    allow overcommitting.

--- src/common/host_memory.cpp
+++ src/common/host_memory.cpp
@@ -393,8 +393,8 @@ public:
         }
 
         // Virtual memory initialization
-        virtual_base = static_cast<u8*>(
-            mmap(nullptr, virtual_size, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+        virtual_base = static_cast<u8*>(mmap(nullptr, virtual_size, PROT_NONE,
+                                             MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0));
         if (virtual_base == MAP_FAILED) {
             LOG_CRITICAL(HW_Memory, "mmap failed: {}", strerror(errno));
             throw std::bad_alloc{};
