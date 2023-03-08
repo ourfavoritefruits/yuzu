d472f4158049869a250939fbcaa69baf4630ed0a
Charles Lombardo <clombardo169@gmail.com>
2023-03-07 20:04:30 -0500

    android: Convert HeaderViewHolder to Kotlin

---
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/ui/viewholder/HeaderViewHolder.kt
@@ -0,0 +1,28 @@
+package org.yuzu.yuzu_emu.features.settings.ui.viewholder
+
+import android.view.View
+import android.widget.TextView
+import org.yuzu.yuzu_emu.R
+import org.yuzu.yuzu_emu.features.settings.model.view.SettingsItem
+import org.yuzu.yuzu_emu.features.settings.ui.SettingsAdapter
+
+class HeaderViewHolder(itemView: View, adapter: SettingsAdapter) :
+    SettingViewHolder(itemView, adapter) {
+    private lateinit var headerName: TextView
+
+    init {
+        itemView.setOnClickListener(null)
+    }
+
+    override fun findViews(root: View) {
+        headerName = root.findViewById(R.id.text_header_name)
+    }
+
+    override fun bind(item: SettingsItem) {
+        headerName.setText(item.nameId)
+    }
+
+    override fun onClick(clicked: View) {
+        // no-op
+    }
+}
