3a5b9ecba2a599aa49ca2a9512036ab16de1f284
Charles Lombardo <clombardo169@gmail.com>
2023-03-07 20:04:04 -0500

    android: Convert CheckBoxSettingViewHolder to Kotlin

---
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/ui/viewholder/CheckBoxSettingViewHolder.kt
@@ -0,0 +1,41 @@
+package org.yuzu.yuzu_emu.features.settings.ui.viewholder
+
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+import org.yuzu.yuzu_emu.R
+import org.yuzu.yuzu_emu.features.settings.model.view.CheckBoxSetting
+import org.yuzu.yuzu_emu.features.settings.model.view.SettingsItem
+import org.yuzu.yuzu_emu.features.settings.ui.SettingsAdapter
+
+class CheckBoxSettingViewHolder(itemView: View, adapter: SettingsAdapter) :
+    SettingViewHolder(itemView, adapter) {
+    private lateinit var item: CheckBoxSetting
+    private lateinit var textSettingName: TextView
+    private lateinit var textSettingDescription: TextView
+    private lateinit var checkbox: CheckBox
+
+    override fun findViews(root: View) {
+        textSettingName = root.findViewById(R.id.text_setting_name)
+        textSettingDescription = root.findViewById(R.id.text_setting_description)
+        checkbox = root.findViewById(R.id.checkbox)
+    }
+
+    override fun bind(item: SettingsItem) {
+        this.item = item as CheckBoxSetting
+        textSettingName.setText(item.nameId)
+        if (item.descriptionId!! > 0) {
+            textSettingDescription.setText(item.descriptionId)
+            textSettingDescription.visibility = View.VISIBLE
+        } else {
+            textSettingDescription.text = ""
+            textSettingDescription.visibility = View.GONE
+        }
+        checkbox.isChecked = this.item.isChecked
+    }
+
+    override fun onClick(clicked: View) {
+        checkbox.toggle()
+        adapter.onBooleanClick(item, bindingAdapterPosition, checkbox.isChecked)
+    }
+}
