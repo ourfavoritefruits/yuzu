fcce7b898f5f8b9821157097ac38612bd60d0792
Charles Lombardo <clombardo169@gmail.com>
2023-03-11 00:35:01 -0500

    android: Convert PlatformGamesPresenter to Kotlin

---
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/ui/platform/PlatformGamesPresenter.kt
@@ -0,0 +1,30 @@
+package org.yuzu.yuzu_emu.ui.platform
+
+import android.database.Cursor
+import org.yuzu.yuzu_emu.YuzuApplication
+import org.yuzu.yuzu_emu.utils.Log
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+class PlatformGamesPresenter(private val view: PlatformGamesView) {
+    fun onCreateView() {
+        loadGames()
+    }
+
+    fun refresh() {
+        Log.debug("[PlatformGamesPresenter] : Refreshing...")
+        loadGames()
+    }
+
+    private fun loadGames() {
+        Log.debug("[PlatformGamesPresenter] : Loading games...")
+        val databaseHelper = YuzuApplication.databaseHelper
+        databaseHelper!!.games
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { games: Cursor? ->
+                Log.debug("[PlatformGamesPresenter] : Load finished, swapping cursor...")
+                view.showGames(games!!)
+            }
+    }
+}
