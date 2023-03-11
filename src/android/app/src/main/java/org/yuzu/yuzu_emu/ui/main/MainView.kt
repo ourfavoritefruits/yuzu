67c2e89d2c3d69df4c983d34500e111fc0b9cd6f
Charles Lombardo <clombardo169@gmail.com>
2023-03-11 00:34:33 -0500

    android: Convert MainView to Kotlin

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/ui/main/MainView.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/ui/main/MainView.kt
@@ -1,25 +1,23 @@
-package org.yuzu.yuzu_emu.ui.main;
+package org.yuzu.yuzu_emu.ui.main
 
 /**
  * Abstraction for the screen that shows on application launch.
  * Implementations will differ primarily to target touch-screen
  * or non-touch screen devices.
  */
-public interface MainView {
+interface MainView {
     /**
      * Pass the view the native library's version string. Displaying
      * it is optional.
      *
      * @param version A string pulled from native code.
      */
-    void setVersionString(String version);
+    fun setVersionString(version: String)
 
     /**
      * Tell the view to refresh its contents.
      */
-    void refresh();
-
-    void launchSettingsActivity(String menuTag);
-
-    void launchFileListActivity(int request);
+    fun refresh()
+    fun launchSettingsActivity(menuTag: String)
+    fun launchFileListActivity(request: Int)
 }
