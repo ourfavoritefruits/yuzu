c02a27ebd2f5a93f87825fa44d1fe0e56e69634a
Charles Lombardo <clombardo169@gmail.com>
2023-03-11 00:35:40 -0500

    android: Convert BiMap to Kotlin

---
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/utils/BiMap.kt
@@ -0,0 +1,22 @@
+package org.yuzu.yuzu_emu.utils
+
+class BiMap<K, V> {
+    private val forward: MutableMap<K, V> = HashMap()
+    private val backward: MutableMap<V, K> = HashMap()
+
+    @Synchronized
+    fun add(key: K, value: V) {
+        forward[key] = value
+        backward[value] = key
+    }
+
+    @Synchronized
+    fun getForward(key: K): V? {
+        return forward[key]
+    }
+
+    @Synchronized
+    fun getBackward(key: V): K? {
+        return backward[key]
+    }
+}
