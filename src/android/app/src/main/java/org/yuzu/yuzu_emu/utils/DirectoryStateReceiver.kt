711aedeaae8c7652c7e8707bc32eb28e8fb82c77
Charles Lombardo <clombardo169@gmail.com>
2023-03-11 00:36:19 -0500

    android: Convert DirectoryStateReceiver to Kotlin

---
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/utils/DirectoryStateReceiver.kt
@@ -0,0 +1,15 @@
+package org.yuzu.yuzu_emu.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import org.yuzu.yuzu_emu.utils.DirectoryInitialization.DirectoryInitializationState
+
+class DirectoryStateReceiver(var callback: (DirectoryInitializationState) -> Unit) :
+    BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val state = intent
+            .getSerializableExtra(DirectoryInitialization.EXTRA_STATE) as DirectoryInitializationState
+        callback.invoke(state)
+    }
+}
