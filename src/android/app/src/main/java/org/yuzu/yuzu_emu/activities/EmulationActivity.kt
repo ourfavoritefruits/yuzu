5807cf1b4ddc9d2b5a7d1db1178555181c00ad89
Charles Lombardo <clombardo169@gmail.com>
2023-03-24 04:21:09 -0400

    android: Fix popup menu going out of bounds

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/activities/EmulationActivity.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/activities/EmulationActivity.kt
@@ -96,7 +96,7 @@ open class EmulationActivity : AppCompatActivity() {
         window.attributes.layoutInDisplayCutoutMode =
             WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN or WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         // It would be nice to use IMMERSIVE_STICKY, but that doesn't show the toolbar.
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
