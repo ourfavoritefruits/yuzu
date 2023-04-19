3e7af5fbd7ae5546b234e39d2ebed7ab47959e2c
Aaron Roney <twitchax@gmail.com>
2023-04-19 16:26:49 +0000

    Fix formatting.

--- src/dedicated_room/yuzu_room.cpp
+++ src/dedicated_room/yuzu_room.cpp
@@ -367,8 +367,8 @@ int main(int argc, char** argv) {
     if (auto room = network.GetRoom().lock()) {
         AnnounceMultiplayerRoom::GameInfo preferred_game_info{.name = preferred_game,
                                                               .id = preferred_game_id};
-        if (!room->Create(room_name, room_description, bind_address, port, password, max_members, username,
-                          preferred_game_info, std::move(verify_backend), ban_list,
+        if (!room->Create(room_name, room_description, bind_address, port, password, max_members,
+                          username, preferred_game_info, std::move(verify_backend), ban_list,
                           enable_yuzu_mods)) {
             LOG_INFO(Network, "Failed to create room: ");
             return -1;
