f9fc9960831da51a417704ed5b41e8705b0c05f1
lat9nq <22451773+lat9nq@users.noreply.github.com>
2023-05-28 21:52:12 +0000

    vk_device_info: Add SPDX data

--- src/yuzu/vk_device_info.cpp
+++ src/yuzu/vk_device_info.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 yuzu Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "video_core/vulkan_common/vulkan_device.h"
 
 #include <vector>
--- src/yuzu/vk_device_info.h
+++ src/yuzu/vk_device_info.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 yuzu Emulator Project
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 #include <string>
