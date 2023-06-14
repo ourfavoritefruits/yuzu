a9e4dddad582d61871076b4310f2c99009afe0db
Wollnashorn <Wollnashorn@users.noreply.github.com>
2023-06-14 11:21:22 +0200

    video_core: Fix default anisotropic heuristic

--- src/video_core/textures/texture.cpp
+++ src/video_core/textures/texture.cpp
@@ -62,10 +62,10 @@ std::array<float, 4> TSCEntry::BorderColor() const noexcept {
 }
 
 float TSCEntry::MaxAnisotropy() const noexcept {
-    const bool suitable_mipmap_filter = Settings::values.use_aggressive_anisotropic_filtering
-                                            ? mipmap_filter != TextureMipmapFilter::None
-                                            : mipmap_filter != TextureMipmapFilter::Linear;
-    if (max_anisotropy == 0 && (depth_compare_enabled.Value() || !suitable_mipmap_filter)) {
+    const bool unsupported_mipmap_filter = Settings::values.use_aggressive_anisotropic_filtering
+                                               ? mipmap_filter == TextureMipmapFilter::None
+                                               : mipmap_filter != TextureMipmapFilter::Linear;
+    if (max_anisotropy == 0 && (depth_compare_enabled.Value() || unsupported_mipmap_filter)) {
         return 1.0f;
     }
     const auto anisotropic_settings = Settings::values.max_anisotropy.GetValue();
