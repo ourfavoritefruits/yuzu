85e175472841689c1f0b60bd8d78f9d2215f3bf8
lat9nq <22451773+lat9nq@users.noreply.github.com>
2023-09-13 13:30:23 -0400

    android/config: Remove uncaught usage of stoul

--- src/android/app/src/main/jni/config.cpp
+++ src/android/app/src/main/jni/config.cpp
@@ -282,7 +282,7 @@ void Config::ReadValues() {
     std::stringstream ss(title_list);
     std::string line;
     while (std::getline(ss, line, '|')) {
-        const auto title_id = std::stoul(line, nullptr, 16);
+        const auto title_id = std::strtoul(line.c_str(), nullptr, 16);
         const auto disabled_list = config->Get("AddOns", "disabled_" + line, "");
 
         std::stringstream inner_ss(disabled_list);
