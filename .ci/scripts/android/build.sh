135b645b3d666f541d5955aaa82af4c480643fde
Charles Lombardo <clombardo169@gmail.com>
2023-10-31 20:43:01 -0400

    ci: android: Use signing key if available
    Lets gradle handle apk signing when available

--- .ci/scripts/android/build.sh
+++ .ci/scripts/android/build.sh
@@ -8,8 +8,17 @@ ccache -s
 
 BUILD_FLAVOR=mainline
 
+if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
+    export ANDROID_KEYSTORE_FILE="${GITHUB_WORKSPACE}/ks.jks"
+    base64 --decode <<< "${ANDROID_KEYSTORE_B64}" > "${ANDROID_KEYSTORE_FILE}"
+fi
+
 cd src/android
 chmod +x ./gradlew
 ./gradlew "assemble${BUILD_FLAVOR}Release" "bundle${BUILD_FLAVOR}Release"
 
 ccache -s
+
+if [ ! -z "${ANDROID_KEYSTORE_B64}" ]; then
+    rm "${ANDROID_KEYSTORE_FILE}"
+fi
