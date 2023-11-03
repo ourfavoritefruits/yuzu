0a83047368b2e0e72535c0239db806d6c229d7e9
Charles Lombardo <clombardo169@gmail.com>
2023-11-03 15:52:01 -0400

    android: Log more system information during startup
    Logs device manufacturer/model, SoC manufacturer/model where available, and the total system memory

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/utils/Log.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/utils/Log.kt
@@ -3,6 +3,8 @@
 
 package org.yuzu.yuzu_emu.utils
 
+import android.os.Build
+
 object Log {
     // Tracks whether we should share the old log or the current log
     var gameLaunched = false
@@ -16,4 +18,14 @@ object Log {
     external fun error(message: String)
 
     external fun critical(message: String)
+
+    fun logDeviceInfo() {
+        info("Device Manufacturer - ${Build.MANUFACTURER}")
+        info("Device Model - ${Build.MODEL}")
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
+            info("SoC Manufacturer - ${Build.SOC_MANUFACTURER}")
+            info("SoC Model - ${Build.SOC_MODEL}")
+        }
+        info("Total System Memory - ${MemoryUtil.getDeviceRAM()}")
+    }
 }
