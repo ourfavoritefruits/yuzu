59011a04a10d20804eb1eb4c8164b64d0f0ca824
Liam <byteslice@airmail.cc>
2024-02-14 00:09:29 -0500

    vi: rewrite IHOSBinderDriver

--- src/core/hle/service/vi/hos_binder_driver.cpp
+++ src/core/hle/service/vi/hos_binder_driver.cpp
@@ -1,64 +1,53 @@
 // SPDX-FileCopyrightText: Copyright 2024 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "core/hle/service/ipc_helpers.h"
+#include "core/hle/service/cmif_serialization.h"
 #include "core/hle/service/nvnflinger/binder.h"
 #include "core/hle/service/nvnflinger/hos_binder_driver_server.h"
 #include "core/hle/service/vi/hos_binder_driver.h"
 
 namespace Service::VI {
 
-IHOSBinderDriver::IHOSBinderDriver(Core::System& system_,
-                                   Nvnflinger::HosBinderDriverServer& server_)
-    : ServiceFramework{system_, "IHOSBinderDriver"}, server(server_) {
+IHOSBinderDriver::IHOSBinderDriver(Core::System& system_, Nvnflinger::HosBinderDriverServer& server)
+    : ServiceFramework{system_, "IHOSBinderDriver"}, m_server(server) {
     static const FunctionInfo functions[] = {
-        {0, &IHOSBinderDriver::TransactParcel, "TransactParcel"},
-        {1, &IHOSBinderDriver::AdjustRefcount, "AdjustRefcount"},
-        {2, &IHOSBinderDriver::GetNativeHandle, "GetNativeHandle"},
-        {3, &IHOSBinderDriver::TransactParcel, "TransactParcelAuto"},
+        {0, C<&IHOSBinderDriver::TransactParcel>, "TransactParcel"},
+        {1, C<&IHOSBinderDriver::AdjustRefcount>, "AdjustRefcount"},
+        {2, C<&IHOSBinderDriver::GetNativeHandle>, "GetNativeHandle"},
+        {3, C<&IHOSBinderDriver::TransactParcelAuto>, "TransactParcelAuto"},
     };
     RegisterHandlers(functions);
 }
 
 IHOSBinderDriver::~IHOSBinderDriver() = default;
 
-void IHOSBinderDriver::TransactParcel(HLERequestContext& ctx) {
-    IPC::RequestParser rp{ctx};
-    const u32 id = rp.Pop<u32>();
-    const auto transaction = static_cast<android::TransactionId>(rp.Pop<u32>());
-    const u32 flags = rp.Pop<u32>();
-
-    LOG_DEBUG(Service_VI, "called. id=0x{:08X} transaction={:X}, flags=0x{:08X}", id, transaction,
+Result IHOSBinderDriver::TransactParcel(s32 binder_id, android::TransactionId transaction_id,
+                                        InBuffer<BufferAttr_HipcMapAlias> parcel_data,
+                                        OutBuffer<BufferAttr_HipcMapAlias> parcel_reply,
+                                        u32 flags) {
+    LOG_DEBUG(Service_VI, "called. id={} transaction={}, flags={}", binder_id, transaction_id,
               flags);
-
-    server.TryGetProducer(id)->Transact(ctx, transaction, flags);
-
-    IPC::ResponseBuilder rb{ctx, 2};
-    rb.Push(ResultSuccess);
+    m_server.TryGetProducer(binder_id)->Transact(transaction_id, flags, parcel_data, parcel_reply);
+    R_SUCCEED();
 }
 
-void IHOSBinderDriver::AdjustRefcount(HLERequestContext& ctx) {
-    IPC::RequestParser rp{ctx};
-    const u32 id = rp.Pop<u32>();
-    const s32 addval = rp.PopRaw<s32>();
-    const u32 type = rp.Pop<u32>();
-
-    LOG_WARNING(Service_VI, "(STUBBED) called id={}, addval={:08X}, type={:08X}", id, addval, type);
-
-    IPC::ResponseBuilder rb{ctx, 2};
-    rb.Push(ResultSuccess);
+Result IHOSBinderDriver::AdjustRefcount(s32 binder_id, s32 addval, s32 type) {
+    LOG_WARNING(Service_VI, "(STUBBED) called id={}, addval={}, type={}", binder_id, addval, type);
+    R_SUCCEED();
 }
 
-void IHOSBinderDriver::GetNativeHandle(HLERequestContext& ctx) {
-    IPC::RequestParser rp{ctx};
-    const u32 id = rp.Pop<u32>();
-    const u32 unknown = rp.Pop<u32>();
+Result IHOSBinderDriver::GetNativeHandle(s32 binder_id, u32 type_id,
+                                         OutCopyHandle<Kernel::KReadableEvent> out_handle) {
+    LOG_WARNING(Service_VI, "(STUBBED) called id={}, type_id={}", binder_id, type_id);
+    *out_handle = &m_server.TryGetProducer(binder_id)->GetNativeHandle();
+    R_SUCCEED();
+}
 
-    LOG_WARNING(Service_VI, "(STUBBED) called id={}, unknown={:08X}", id, unknown);
-
-    IPC::ResponseBuilder rb{ctx, 2, 1};
-    rb.Push(ResultSuccess);
-    rb.PushCopyObjects(server.TryGetProducer(id)->GetNativeHandle());
+Result IHOSBinderDriver::TransactParcelAuto(s32 binder_id, android::TransactionId transaction_id,
+                                            InBuffer<BufferAttr_HipcAutoSelect> parcel_data,
+                                            OutBuffer<BufferAttr_HipcAutoSelect> parcel_reply,
+                                            u32 flags) {
+    R_RETURN(this->TransactParcel(binder_id, transaction_id, parcel_data, parcel_reply, flags));
 }
 
 } // namespace Service::VI
