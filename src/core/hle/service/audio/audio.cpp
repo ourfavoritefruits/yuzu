4cbafc1ef60e7edcf736c27607b840c869918f83
Narr the Reg <juangerman-13@hotmail.com>
2024-02-14 19:51:07 -0600

    service: audio: Rewrite IAudioController to new IPC

--- src/core/hle/service/audio/audio.cpp
+++ src/core/hle/service/audio/audio.cpp
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "core/core.h"
-#include "core/hle/service/audio/audctl.h"
 #include "core/hle/service/audio/audin_u.h"
 #include "core/hle/service/audio/audio.h"
+#include "core/hle/service/audio/audio_controller.h"
 #include "core/hle/service/audio/audout_u.h"
 #include "core/hle/service/audio/audrec_a.h"
 #include "core/hle/service/audio/audrec_u.h"
@@ -18,7 +18,7 @@ namespace Service::Audio {
 void LoopProcess(Core::System& system) {
     auto server_manager = std::make_unique<ServerManager>(system);
 
-    server_manager->RegisterNamedService("audctl", std::make_shared<AudCtl>(system));
+    server_manager->RegisterNamedService("audctl", std::make_shared<IAudioController>(system));
     server_manager->RegisterNamedService("audout:u", std::make_shared<AudOutU>(system));
     server_manager->RegisterNamedService("audin:u", std::make_shared<AudInU>(system));
     server_manager->RegisterNamedService("audrec:a", std::make_shared<AudRecA>(system));
