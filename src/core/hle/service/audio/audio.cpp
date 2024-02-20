2a2c92f1819f44a86f452654398d0fc16806c438
Liam <byteslice@airmail.cc>
2024-02-19 21:45:48 -0500

    audio: rewrite IAudioInManager

--- src/core/hle/service/audio/audio.cpp
+++ src/core/hle/service/audio/audio.cpp
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "core/core.h"
-#include "core/hle/service/audio/audin_u.h"
 #include "core/hle/service/audio/audio.h"
 #include "core/hle/service/audio/audio_controller.h"
+#include "core/hle/service/audio/audio_in_manager.h"
 #include "core/hle/service/audio/audout_u.h"
 #include "core/hle/service/audio/audrec_a.h"
 #include "core/hle/service/audio/audrec_u.h"
@@ -19,8 +19,8 @@ void LoopProcess(Core::System& system) {
     auto server_manager = std::make_unique<ServerManager>(system);
 
     server_manager->RegisterNamedService("audctl", std::make_shared<IAudioController>(system));
+    server_manager->RegisterNamedService("audin:u", std::make_shared<IAudioInManager>(system));
     server_manager->RegisterNamedService("audout:u", std::make_shared<AudOutU>(system));
-    server_manager->RegisterNamedService("audin:u", std::make_shared<AudInU>(system));
     server_manager->RegisterNamedService("audrec:a", std::make_shared<AudRecA>(system));
     server_manager->RegisterNamedService("audrec:u", std::make_shared<AudRecU>(system));
     server_manager->RegisterNamedService("audren:u", std::make_shared<AudRenU>(system));
