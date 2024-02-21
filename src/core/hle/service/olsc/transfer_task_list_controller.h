6b956a695196d73c38fd717c516f3ae27e0f2387
Liam <byteslice@airmail.cc>
2024-02-21 18:36:17 -0500

    olsc: rewrite ITransferTaskListController

--- src/core/hle/service/olsc/transfer_task_list_controller.h
+++ src/core/hle/service/olsc/transfer_task_list_controller.h
@@ -1,9 +1,12 @@
 // SPDX-FileCopyrightText: Copyright 2024 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include "core/hle/service/cmif_types.h"
 #include "core/hle/service/service.h"
 
 namespace Service::OLSC {
+
+class INativeHandleHolder;
 
 class ITransferTaskListController final : public ServiceFramework<ITransferTaskListController> {
 public:
@@ -11,7 +14,7 @@ public:
     ~ITransferTaskListController() override;
 
 private:
-    void GetNativeHandleHolder(HLERequestContext& ctx);
+    Result GetNativeHandleHolder(Out<SharedPointer<INativeHandleHolder>> out_holder);
 };
 
 } // namespace Service::OLSC
